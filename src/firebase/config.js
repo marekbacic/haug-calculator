422c615fc06fe7fda49de1fc01bedc5e8874009c
marekbacic marek <marekbacic@gmail.com>
2025-03-13 11:10:01 +0000

    Dodanie plików aplikacji

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBODpeDXianUxhYBJM1-O3j19qVGmA6QIY",
+  authDomain: "haugchemie1.firebaseapp.com",
+  projectId: "haugchemie1",
+  storageBucket: "haugchemie1.firebasestorage.app",
+  messagingSenderId: "873970413604",
+  appId: "1:873970413604:web:acd25cbdba08576a45e8b1"
+};
+
+// Inicjalizacja Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
